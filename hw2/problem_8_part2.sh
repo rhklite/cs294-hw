5ae7e91c3f7a347f413b7332e6b82c9632574bd5
richard <hanhu@mie.utoronto.ca>
2020-03-19 03:41:11 -0400

    fixed save file name

--- hw2/problem_8_part2.sh
+++ hw2/problem_8_part2.sh
@@ -2,6 +2,6 @@
 python train_pg_f18.py HalfCheetah-v2 -ep 150 --discount 0.95 -n 100 -e 3 -l 2 -s 32 -b 50000 -lr 0.02 --exp_name hc_b50000_r0.02 --dir problem_8_part2
 python train_pg_f18.py HalfCheetah-v2 -ep 150 --discount 0.95 -n 100 -e 3 -l 2 -s 32 -b 50000 -lr 0.02 -rtg --exp_name hc_b50000_r0.02_rtg --dir problem_8_part2
 python train_pg_f18.py HalfCheetah-v2 -ep 150 --discount 0.95 -n 100 -e 3 -l 2 -s 32 -b 50000 -lr 0.02 --nn_baseline --exp_name hc_b50000_r0.02_bl --dir problem_8_part2
-python train_pg_f18.py HalfCheetah-v2 -ep 150 --discount 0.95 -n 100 -e 3 -l 2 -s 32 -b 50000 -lr 0.02 -rtg --nn_baseline --exp_name hc_b50000_r0.02 --dir_rtg_bl problem_8_part2 
+python train_pg_f18.py HalfCheetah-v2 -ep 150 --discount 0.95 -n 100 -e 3 -l 2 -s 32 -b 50000 -lr 0.02 -rtg --nn_baseline --exp_name hc_b50000_r0.02_rtg_bl --dir_rtg_bl problem_8_part2 
 
 notify-send "cs294" "Problem 8 Training Complete"
